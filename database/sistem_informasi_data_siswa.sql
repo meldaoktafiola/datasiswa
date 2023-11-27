66f9c9c781ba4644936c3f1dff5ea8c0c943d93a
meldaoktafiola <fiolala36@gmail.com>
2023-11-27 14:55:06 +0700

    first commit

---
+++ database/sistem_informasi_data_siswa.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2023 at 08:20 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistem_informasi_data_siswa`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getsiswa` ()   BEGIN
+
+SELECT * FROM siswa;
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jurusan`
+--
+
+CREATE TABLE `jurusan` (
+  `id_jurusan` int(11) NOT NULL,
+  `nama_jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jurusan`
+--
+
+INSERT INTO `jurusan` (`id_jurusan`, `nama_jurusan`) VALUES
+(3, 'TITL'),
+(5, 'TPE'),
+(6, 'TKJ'),
+(7, 'RPL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `id_jurusan` int(11) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `id_jurusan`, `nama_kelas`) VALUES
+(5, 3, 'XII'),
+(6, 7, 'XII'),
+(7, 3, 'XI'),
+(8, 3, 'X'),
+(10, 7, 'XI'),
+(11, 3, 'XI');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` char(10) NOT NULL,
+  `nis` char(4) NOT NULL,
+  `nama` varchar(32) NOT NULL,
+  `tempat_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(32) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `id_kelas`) VALUES
+('048284939', '4683', 'shelly', 'seoul', '2000-08-23', 'Perempuan', 6),
+('32142', '0447', 'Owen', 'metro', '2006-01-11', 'Laki-Laki', 8),
+('785846535', '5842', 'Fio', 'metro', '2006-10-29', 'Perempuan', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nama_lengkap` varchar(32) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_lengkap`, `foto`) VALUES
+(1, 'Melda', '202cb962ac59075b964b07152d234b70', 'Shelly', '1949045716_images.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `jurusan`
+--
+ALTER TABLE `jurusan`
+  ADD PRIMARY KEY (`id_jurusan`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`),
+  ADD KEY `id_jurusan` (`id_jurusan`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD UNIQUE KEY `nis` (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jurusan`
+--
+ALTER TABLE `jurusan`
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tambah-siswa.php
@@ -0,0 +1,83 @@
+<?php
+if (isset($_POST['nisn'])) {
+    $nisn = $_POST['nisn'];
+    $nis = $_POST['nis'];
+    $nama = $_POST['nama_siswa'];
+    $tempat_lahir = $_POST['tempat_lahir'];
+    $tanggal_lahir = $_POST['tanggal_lahir'];
+    $jenis_kelamin  = $_POST['jenis_kelamin'];
+    $id_kelas = $_POST['id_kelas'];
+
+    $query = mysqli_query($koneksi, "INSERT INTO siswa (nisn,nis,nama,tempat_lahir,tanggal_lahir,jenis_kelamin,id_kelas) VALUES('$nisn','$nis','$nama','$tempat_lahir','$tanggal_lahir','$jenis_kelamin','$id_kelas')");
+
+    if ($query) {
+        echo '<script>alert("Data Berhasil di Tambah");location.href="?page=siswa";</script>';
+    }
+}
+?>
+
+<h1 class="h3 mb-3">Tambah Siswa</h1>
+
+					<div class="row">
+                        <div class="col-12">
+                            <div class="card flex-fill">
+                                <div class="card-body">
+                                    <form method="post">
+                                        <div class="mb-3">
+                                            <label class="form-label">NISN</label>
+                                            <div class="col-12">
+                                                <input type="text" name="nisn" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">NIS</label>
+                                            <div class="col-12">
+                                                <input type="text" name="nis" class="form-control">
+                                            </div>
+                                        </div>
+                                            <div class="mb-3">
+                                                <label class="form-label">Nama Siswa</label>
+                                                <div class="col-12">
+                                                    <input type="text" name="nama_siswa" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label class="form-label">Tempat Lahir</label>
+                                                <div class="col-12">
+                                                    <input type="text" name="tempat_lahir" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label class="form-label">Tanggal lahir</label>
+                                                <div class="col-12">
+                                                    <input type="date" name="tanggal_lahir" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label class="form-label">Jenis Kelamin</label>
+                                                <select name="jenis_kelamin" class="form-select">
+                                                    <option value="Perempuan">Perempuan</option>
+                                                        <option value="Laki-Laki">Laki-Laki</option>
+                                                </select>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label class="form-label">Kelas Dan Jurusan</label>
+                                                <select name="id_kelas" class="form-select">
+                                                    <?php 
+                                                    $query = mysqli_query($koneksi, "SELECT * FROM kelas INNER JOIN jurusan ON kelas.id_jurusan=jurusan.id_jurusan");
+                                                    while ($data = mysqli_fetch_array($query)) {
+                                                        ?>
+                                                        <option value="<?php echo $data['id_kelas'] ?>"><?php echo $data['nama_kelas'] ?> - <?php echo $data['nama_jurusan']?></option>
+                                                        <?php
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        <div class="mb-3">
+                                            <button class="btn btn-primary">Simpan</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
